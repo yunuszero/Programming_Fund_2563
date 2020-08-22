ad295df2f44a84722636273ddf61316310bb15ce
yunus deemad <yunuszero@hotmail.com>
2020-08-22 20:04:02 +0700

    change max array index  to 20

--- matrixElementsSum/matrixEsum.cpp
+++ matrixElementsSum/matrixEsum.cpp
@@ -1,20 +1,26 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int maini() {
-    int matrix[15][15];
+int main() {
+    int matrix[20][20];
     int n, m;
+    scanf_s("%d %d", &n, &m);
+    
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             scanf_s("%d", &matrix[i][j]);
         }
     }
     int sum = 0;
-    for (int i = 0; i < sizeof(matrix[0])/ sizeof(matrix[0][0]); i++) {
+    for (int i = 0; i < m; i++) {
         sum += matrix[0][i];
-        for (int j = 1; j < sizeof(matrix) / sizeof(matrix[0]); j++) {
+        for (int j = 1; j < n; j++) {
             if (matrix[j - 1][i])
                 sum += matrix[j][i];
             else break;
         }
     }
+    printf("%d", sum);
+
+    return 0;
 }
