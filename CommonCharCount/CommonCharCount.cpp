664a65303189780f9ddeed4c7cf61dbd987aa36d
yunus deemad <yunuszero@hotmail.com>
2020-08-22 10:46:26 +0700

    change iostream to <stdio.h> and <string.h>

--- CommonCharCount/CommonCharCount.cpp
+++ CommonCharCount/CommonCharCount.cpp
@@ -1,9 +1,11 @@
-#include <iostream>
-int commonCharCount(std::string s1, std::string s2) {
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+int commonCharCount(char* s1, char* s2) {
     int mem[15] = { 0 };
     int cnt = 0;
-    for (int i = 0; i < s1.length(); i++) {
-        for (int j = 0; j < s2.length(); j++) {
+    for (int i = 0; i < strlen(s1); i++) {
+        for (int j = 0; j < strlen(s2); j++) {
             if (s1[i] == s2[j] && mem[j] == 0) {
                 cnt++;
                 mem[j]++;
@@ -15,8 +17,8 @@ int commonCharCount(std::string s1, std::string s2) {
 }
 
 int main() {
-    std::string n, m;
-    std::cin >> n >>m;
+    char n[15], m[15];
+    scanf("%[a-z] %[a-z]", n, m);
     printf("%d", commonCharCount(n, m));
 
     return 0;
