24901cc97227da6147488c0f1dc73f15ce7b9e9f
yunus deemad <yunuszero@hotmail.com>
2020-11-01 18:48:59 +0700

    1/11

--- Dancing/Source.cpp
+++ Dancing/Source.cpp
@@ -16,19 +16,22 @@ int main() {
 	int n;
 	struct member M[10], F[10];
 	scanf_s("%d", &n);
+	printf("Male:\n");
 	for (int i = 0; i < n; i++) {
 		cin >> M[i].name >> M[i].height;
 	}
 	sort(M, n);
+	printf("Female:\n");
 	for (int i = 0; i < n; i++) {
 		cin >> F[i].name >> F[i].height;
 	}
 	sort(F, n);
 	cout << selected(M, F, n) << endl;
+	// Printed output.
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++) {
-			if (check[j][i]) {
+			if (check[i][j]) {
 				printf("%s %s\n", M[i].name, F[j].name);
 			}
 		}
@@ -39,7 +42,7 @@ int main() {
 void sort(struct member *x, int n) {
 	struct member temp;
 	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n-i; j++) {
+		for (int j = 0; j < n; j++) {
 			if (x[j].height < x[j+1].height) {
 				temp = x[j];
 				x[j] = x[j+1];
@@ -53,27 +56,17 @@ int selected(struct member *m, struct member *f, int n) {
 	bool used[10] = { 0,0,0,0,0,0,0,0,0,0 };
 	int cnt = 0;
 	for (int i = 0; i < n; i++) {
-		int temp = 6, chk = -1, pass=0;
+		int chk = -1;
 		for (int j = 0; j < n; j++) {
-			if (f[i].height < m[j].height && abs(m[j].height - f[i].height) <= 5 && used[j] == 0) {
-				if (temp > abs(m[j].height - f[i].height)) {
-					temp = abs(m[j].height - f[i].height);
-					chk = j;
-					pass = 1;
-				}
+			if (m[i].height >= f[j].height && abs(m[i].height - f[j].height) <= 5 && used[j] == 0) {
+				chk = j;
 			}
-			else if (abs(m[j].height - f[i].height) <= 5 && used[j] == 0 && pass == 0) {
-				if (temp > abs(m[j].height - f[i].height)) {
-					temp = abs(m[j].height - f[i].height);
-					chk = j;
-					cout << temp << " " << chk << endl;
-				}
+			if (chk != -1) {
+				check[i][chk] = true;
+				used[chk] = true;
+				cnt++;
+				break;
 			}
-		}
-		if (chk != -1) {
-			check[i][chk] = true;
-			used[chk] = true;
-			cnt++;
 		}
 	}
 	return cnt;
